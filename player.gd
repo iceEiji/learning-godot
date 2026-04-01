8aad1779d8bd9ef7dfafb50ea46c9fac5e684a10
iceEiji <ice.eiji.work@gmail.com>
2026-04-01 20:34:16 +0900

    feat: 衝突判定

--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed: int = 400
 var screen_size
@@ -37,3 +38,15 @@ func _process(delta: float) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
