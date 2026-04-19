250748786606ef4ef6ad5ccc772665fd3816d202
iceEiji <ice.eiji.work@gmail.com>
2026-04-19 13:57:32 +0900

    Add Music and DeathSound

--- main.gd
+++ main.gd
@@ -13,6 +13,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game() -> void:
 	score = 0
@@ -21,6 +23,7 @@ func new_game() -> void:
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
